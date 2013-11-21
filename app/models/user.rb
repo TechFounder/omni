87d976de647050ca754b7da7fc6216d3a53961fc
TechFounder <techfounder@outlook.com>
2013-11-20 23:09:10 -0500

    Fix Twitter regiestration form new to the correct attribute.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
+#  name                   :string(255)      default(""), not null
 #  email                  :string(255)      default(""), not null
 #  encrypted_password     :string(255)      default(""), not null
 #  reset_password_token   :string(255)
@@ -13,6 +14,8 @@
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string(255)
 #  last_sign_in_ip        :string(255)
+#  provider               :string(255)
+#  uid                    :string(255)
 #  created_at             :datetime
 #  updated_at             :datetime
 #
